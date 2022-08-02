cdaa3c6deab4a9403b33b69d04a33b9d56a4b403
hugehoo <tbnsok40@gmail.com>
2022-08-03 01:45:08 +0900

    feature/ todo post api

--- src/main/java/project/todo/controller/TodoApi.java
+++ src/main/java/project/todo/controller/TodoApi.java
@@ -1,2 +1,34 @@
-package project.todo;public class TodoApi {
+package project.todo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import project.todo.BaseController;
+import project.todo.domain.Todo;
+import project.todo.dto.TodoDto;
+import project.todo.repository.TodoRepository;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("todo")
+@RequiredArgsConstructor
+public class TodoApi extends BaseController {
+
+    private final TodoRepository repository;
+
+    @GetMapping
+    public String sample() {
+        List<Todo> all = repository.findAll();
+        for (Todo todo : all) {
+            System.out.println(todo.getTodos());
+        }
+        return "string";
+    }
+
+    @PostMapping
+    public void createTodos(@RequestBody TodoDto todoDto) {
+        Todo todo = modelMapper.map(todoDto, Todo.class); // Entity 에 setter 가 없으면 매핑이 안된다.  -> 그럼 modelMapper 사용하려면 필연적으로 entity 의 setter 열어야하네 ?
+        repository.save(todo);
+    }
 }
---
+++ src/main/java/project/todo/dto/TodoDto.java
@@ -0,0 +1,12 @@
+package project.todo.dto;
+
+
+import lombok.Data;
+
+@Data
+public class TodoDto {
+
+    private String todos;
+    private boolean done;
+
+}
